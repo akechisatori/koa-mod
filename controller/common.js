98319029c4349f4d0e7d7e77bc4cb177d2ce5598
Akechi Satori <i@menhera.io>
2019-04-28 04:42:45 +0800

    add usage

--- controller/common.js
+++ controller/common.js
@@ -1,9 +1,10 @@
 const axios = require('axios')
 
 module.exports = {
-    ip: () => {
+    ip: (ctx, query) => {
         return new Promise((resolve, reject) => {
-            axios.get('https://api.ip.sb/geoip').then(res => {
+            let ip = query.ip;
+            axios.get('https://api.ip.sb/geoip' + (ip ? '/' + ip : '')).then(res => {
                 resolve(res.data);
             }).catch(err => {
                 reject(err);
