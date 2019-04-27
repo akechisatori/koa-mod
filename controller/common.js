c6472f03bd657f748387665404ce89217abf18b8
Akechi Satori <i@menhera.io>
2019-04-28 05:20:08 +0800

    add next() method test

--- controller/common.js
+++ controller/common.js
@@ -1,11 +1,16 @@
 const axios = require('axios')
 
 module.exports = {
-    ip: (ctx, query) => {
+    ip: (ctx, query, next) => {
         return new Promise((resolve, reject) => {
             let ip = query.ip;
             axios.get('https://api.ip.sb/geoip' + (ip ? '/' + ip : '')).then(res => {
                 resolve(res.data);
+                next().then(() => {
+                    setTimeout(() => {
+                        console.log("next() method test")
+                    }, 2000);
+                })
             }).catch(err => {
                 reject(err);
             });
