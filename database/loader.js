eb8add7f316a4c1db135caadd798dcc3631fb00a
Akechi Satori <i@menhera.io>
2019-05-10 02:36:42 +0800

    fix async bug

--- database/loader.js
+++ database/loader.js
@@ -3,28 +3,21 @@ const sequelize = require('./core/sequelize');
 const fs = require('fs');
 const path = require('path');
 
-async function LoadModel() {
-    return new Promise((resolve, reject) => {
-        const Models = {};
-        let files = fs.readdirSync(__dirname + '/models/');
-        files.forEach(filestr => {
-            const file = path.parse(filestr);
-            if (file.ext === '.js') {
-                const ModelObject = sequelize.import(`${__dirname}/models/${file.base}`);
-                Models[file.name] = ModelObject;
-            }
-        });
-        resolve(Models);
-    });
-}
-
-LoadModel().then(Models => {
-    Object.keys(Models).forEach((value, index) => {
-        Models[value].sync({
-            alter: true
-        }).then(() => {
-            console.log(`Table '${value}' Sync Complete`);
-        });
-    });
-    module.exports = Models;
+const Models = {};
+let files = fs.readdirSync(__dirname + '/models/');
+files.forEach(filestr => {
+    const file = path.parse(filestr);
+    if (file.ext === '.js') {
+        const ModelObject = sequelize.import(`${__dirname}/models/${file.base}`);
+        Models[file.name] = ModelObject;
+    }
 });
+
+Object.keys(Models).forEach((value, index) => {
+    Models[value].sync({
+        alter: true
+    }).then(() => {
+        console.log(`Table '${value}' Sync Complete`);
+    });
+});
+module.exports = Models;
